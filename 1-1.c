7ff55f9cd657583df33e42aa3013ddf6c2554e55
170225 SEKIYA YUKI <k756880@tc2101.local>
2019-04-05 16:39:02 +0900

    課題1-1 完成

--- 1-1.c
+++ 1-1.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello World\n");
+	FILE * fp;
+	int number;
+
+	// ファイルオープン
+	fp = fopen("./01number.txt", "r");
+	// エラー処理
+	if(fp == NULL) {
+		printf("ファイルを開けません\n");
+		return -1;
+	}
+
+	// ストリームから取り出し
+	while(fscanf(fp, "%d", &number) != EOF) {
+		// 出力
+		printf("%d ", number);
+	}
+	printf("\n");
+
+	// ファイルクローズ
+	fclose(fp);
+
 	return 0;
 }
