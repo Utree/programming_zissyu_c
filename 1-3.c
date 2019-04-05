b3dc44ab8bfd660fbf1a0eaa196eab8682bfca6f
170225 SEKIYA YUKI <k756880@tc2101.local>
2019-04-05 17:03:12 +0900

    課題1-3 完成

---
+++ 1-3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define NUMBER 15
+
+int main() {
+	FILE * fp; // ファイルのストリーム
+	int number[NUMBER]; // ファイル入力保存用
+	int i = 0; // カウンタ
+	int max; // 最大値保存用
+
+	// ファイルオープン
+	fp = fopen("./01number.txt", "r");
+	// エラー処理
+	if(fp == NULL) {
+		printf("ファイルを開けません\n");
+		return -1;
+	}
+
+	// ストリームから取り出し
+	while(fscanf(fp, "%d", &number[i]) != EOF) {
+		// 保存
+		i++; 
+	}
+
+	// ファイルクローズ
+	fclose(fp);
+	
+	
+	// 変数maxの初期化
+	max = number[0];
+
+	// 最大値を算出
+	for(i=0;i<NUMBER;i++) {
+		if(max < number[i]) max = number[i];	
+	}
+
+	// ファイル出力
+	fp = fopen("./01maxnumber.txt", "w");
+	
+	fprintf(fp, "%d\n", max);
+
+	fclose(fp);
+
+	return 0;
+}
